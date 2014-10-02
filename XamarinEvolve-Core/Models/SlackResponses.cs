dd216c3ab76ec9a6108bf89928ae676673c4e985
Paul Betts <paul@paulbetts.org>
2014-10-02 16:36:10 -0700

    Map the models we'll need for logging in

---
+++ XamarinEvolve-Core/Models/SlackResponses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+// CoolStuff: You don't have to actually write this code at all, just go to
+// json2csharp.com
+namespace XamarinEvolve.Core.Models
+{
+    public class TeamWithUser
+    {
+        public string url { get; set; }
+        public string team { get; set; }
+        public string user { get; set; }
+        public string team_id { get; set; }
+        public string user_id { get; set; }
+    }
+
+    public class AvailableTeamList
+    {
+        public bool ok { get; set; }
+        public string email { get; set; }
+        public string domain { get; set; }
+        public List<TeamWithUser> users { get; set; }
+        public string create { get; set; }
+    }
+
+    public class AuthenticationResult
+    {
+        public bool ok { get; set; }
+        public string token { get; set; }
+        public string user { get; set; }
+        public string team { get; set; }
+    }
+}
